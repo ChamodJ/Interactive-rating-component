3b3caeaec96082b3d0a8924fa90e4d4ef0683295
ChamodJ <121800679+ChamodJ@users.noreply.github.com>
2023-03-21 01:23:19 +0530

    Add java script

---
+++ index.js
@@ -0,0 +1,22 @@
+let rateEl = document.getElementById("rate-el")
+let massege = ""
+
+function button1() {
+    massege = "You selected 1 out of 5"
+}
+
+function button2() {
+    massege = "You selected 2 out of 5"
+}
+
+function button3() {
+    massege = "You selected 3 out of 5"
+}
+
+function button4() {
+    massege = "You selected 4 out of 5"
+}
+
+function button5() {
+    massege = "You selected 5 out of 5"
+}
