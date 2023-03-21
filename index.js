94f4441d378c51e2c0b76f5d33534c9a814fe47f
ChamodJ <121800679+ChamodJ@users.noreply.github.com>
2023-03-21 19:59:31 +0530

    Update

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
-let rateEl = document.getElementById("rate-el")
-let massege = ""
 
+
+/*
 function button1() {
     massege = "You selected 1 out of 5"
+    localStorage.setItem(massege)
 }
 
 function button2() {
     massege = "You selected 2 out of 5"
+    localStorage.setItem(massege)
 }
 
 function button3() {
     massege = "You selected 3 out of 5"
+    localStorage.setItem(massege)
 }
 
 function button4() {
     massege = "You selected 4 out of 5"
+    localStorage.setItem(massege)
 }
 
 function button5() {
     massege = "You selected 5 out of 5"
+    localStorage.setItem(massege)
+}
+
+function submit() {
+rateEl.textContent = localStorage.getItem(massege)
+}
+*/
+document.getElementById("").addEventListener("click" , massege)
+
+function massege() {
+    document.getElementById("rate-el").innerHTML = ("You selected 1 out of 5")
 }
